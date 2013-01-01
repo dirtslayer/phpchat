e6a0d81e66b8606628c730b8937795ce0b6038fe
semisided1 <dirtslayer@gmail.com>
2012-12-31 19:41:11 -0700

    tidy css some more, clean up color picker

--- chatxml.php
+++ chatxml.php
@@ -19,10 +19,15 @@
 <div class="send">
 
 <form name="input" action="chat_add.php" method="post">
-<input type="text" value="<?php echo $_SESSION['user_name'] ?>" name="user" maxlength="10" size="10">
-<a id="colorpick" href="colorpick" style="color:<?php echo $_SESSION['color'] ?>">Says</a> <input class="txtinput" autofocus="true" type="text" name="says" maxlength="170" >
-<input style="display: none" id="colorhex" type="text" name="color" maxlength="10" value="<?php echo $_SESSION['color'] ?>" >
-<input type="submit" value="Submit">
+
+<input type="text" style="display:none" id="colorhex"  name="color" value="<?php echo $_SESSION['color'] ?>" maxlength="10" >
+
+<input class="txtinput" autofocus="true" type="text" name="says" maxlength="170" >
+<input class="submitbtn" type="submit" value="Submit" >
+
+<a id="namepick" href="namepick" style="color:<?php echo $_SESSION['color'] ?>">change your name, <?php echo $_SESSION['user_name'] ?> </a> 
+<input type="text" style="display:none" id="nameinput" name="user"  value="<?php echo $_SESSION['user_name'] ?>"  maxlength="10" >
+<a id="colorpick" href="colorpick" style="color:<?php echo $_SESSION['color'] ?>">select your color</a> 
 </form> 
 
 </div>
@@ -37,27 +42,40 @@
 (function($){ $.fn.autofocus=function(){return (this.first().autofocus!==true)?this.focus():this;};})(jQuery);
 var timehandler = function () {
     $('.chat').load("./chat.php?r=" + Math.random()*99999 + " .msgs ");
-    $("html, body").animate({ scrollTop: $("#bang").offset().top }, 2000);
+   // $("html, body").animate({ scrollTop: $("#bang").offset().top }, 2000);
+   $("html, body").animate({ scrollTop: $(document).height() - $(window).height()  }, 2000);
 } 
 var anotherhandler = function() { 
 	$('[autofocus]').autofocus();
     $('.chat').load("./chat.php?r=" + Math.random()*99999 + " .msgs ", function() {
     	// $("#bang").offsetParent().scrollTop($("#bang").offset().top);
-    	$("html, body").animate({ scrollTop: $("#bang").offset().top }, 1);
+    	// $("html, body").animate({ scrollTop: $("#bang").offset().top }, 1);
+    	$("html, body").animate({ scrollTop: $(document).height() - $(window).height()  }, 2000);
     	
     });
    
+ //  $(window).height();   // returns height of browser viewport
+//$(document).height(); // returns height of HTML document
    
    $("#colorpick").click( function (){
    		$("#w3cpicker").load("./w3cpicker.html", function () {
    				// $("#bang").offsetParent().scrollTop($("#bang").offset().top);
-   				$("html, body").animate({ scrollTop: $("#bang").offset().top }, 2000);
+   				//$("html, body").animate({ scrollTop: $("#bang").offset().top }, 2000);
+   				$("html, body").animate({ scrollTop: $(document).height() - $(window).height()  }, 2000);
+   				applyclickhandler();
    		});
    		return false;
    });
    
+   $("#namepick").click( function (){
+   		$("#w3cpicker").load("./namepicker.html", function () {
+   				// $("#bang").offsetParent().scrollTop($("#bang").offset().top);
+   				$("#nameinput").fadeIn("slow");
+   		});
+   		return false;
+   });
    
-    setInterval( timehandler, 5000); 
+    setInterval( timehandler, 10000); 
 } 
 $(document).ready( anotherhandler );
 
